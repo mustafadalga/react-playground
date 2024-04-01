0df1a21819445afc2dfbe6ab0db07c034ed28f1d
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-04-01 23:28:20 +0300

    Add modal transition effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { lazy, Suspense, useEffect } from "react";
+import useModalStore from "@/hooks/useModalStore.ts";
+
+const Modal = lazy(() => import("@/components/Modal.tsx"))
+
+function Loading() {
+    return <h2>🌀 Loading...</h2>;
+}
+
 
 function App() {
+    const {
+        isModalOpen, toggleModalVisibility
+    } = useModalStore();
 
-  return (
-    <>
+    useEffect(() => {
+        console.log(isModalOpen)
 
-    </>
-  )
+    }, [ isModalOpen ]);
+
+
+    return (
+        <main className="grid gap-4">
+            <button type="button"
+                    onClick={() => toggleModalVisibility(true)}
+                    className="w-60 bg-blue-500 text-white text-center rounded-lg font-bold text-xs lg:text-sm xl:text-base h-9 xl:h11 2xl:h-12">
+                Open Modal
+            </button>
+
+            <Suspense fallback={<Loading/>}>
+                {isModalOpen && <Modal/>}
+            </Suspense>
+        </main>
+    )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                "modal-slide-down": "modal-slide-down .5s ease-in-out",
+                "modal-slide-up": "modal-slide-up .5s ease-in-out",
+            },
+            keyframes: {
+                "modal-slide-down": {
+                    'from': {
+                        transform: "translateY(-100%)"
+                    },
+                    'to': {
+                        transform: "translateY(0)"
+                    },
+                },
+                "modal-slide-up": {
+                    'from': {
+                        transform: "translateY(0)"
+                    },
+                    'to': {
+                        transform: "translateY(-100%)"
+                    },
+                },
+            }
+        },
+    },
+    plugins: [],
 }
