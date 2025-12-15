12bec8be849bee7b6357b4372395873a1b03b824
Mustafa Dalga <mustafadalgaa@gmail.com>
2025-12-15 22:21:06 +0200

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain=import.meta.env.VITE_AUTH0_DOMAIN
+const clientid=import.meta.env.VITE_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+          domain={domain}
+          clientId={clientid}
+          authorizationParams={{
+              redirect_uri: window.location.origin
+          }}
+      >
+          <App />
+      </Auth0Provider>
   </React.StrictMode>,
 )
